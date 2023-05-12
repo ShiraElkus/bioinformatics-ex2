d47575a28eff56a33ba2aeebaf06daeb50b893ba
idodd <idoddii@gmail.com>
2023-05-12 16:14:38 +0300

    Making our own algorithm

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+
+CIPHERFILE = "enc.txt"
+
+LETTERFREQ = "Letter_Freq.txt"
+
+LETTERFREQ2 = "Letter2_Freq.txt"
+
+DICTFILE = "dict.txt"
+
+POPULATION_COUNT = 0
+
+CARRY_POPULATION = 0
+
+MUTATIONS = 0
+
+CROSSOVERS = 0
+
+def decode():
+
+
+if __name__ == "__main__":
+    decode()
